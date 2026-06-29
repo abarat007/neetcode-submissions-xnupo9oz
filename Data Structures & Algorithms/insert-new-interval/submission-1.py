3f0ab2d596c6a0f80f673706d559cf7f565a6b52
Abhinabha Barat <ab3299@cornell.edu>
2026-06-29 11:04:48 -0400

    Add: insert-new-interval - submission-1

---
+++ Data Structures & Algorithms/insert-new-interval/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        # Edge Case 1: if newInterval[1] < intervals[0][0], then add newInterval to the front of intervals
+
+        # Edge Case 2: if newInterval[0] > intervals[-1][1], then append newInterval to intervals
+
+        # if 
+        # new_left = 
+        # new_right = 
+        res = []
+        for i in range(len(intervals)):
+            # Edge Case 1:
+            if intervals[i][0] > newInterval[1]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            # Edge Case 2:
+            elif intervals[i][1] < newInterval[0]:
+                res.append(intervals[i])
+            else:
+                newInterval = [(min(newInterval[0], intervals[i][0])), (max(newInterval[1], intervals[i][1]))]
+                
+        res.append(newInterval)
+        return res
+
+        
+    
+
+
+
+        
+        

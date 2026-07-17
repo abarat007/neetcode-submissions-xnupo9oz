7c6de56154d89fb2e238c83a99343a6cb8c0e7f4
Abhinabha Barat <ab3299@cornell.edu>
2026-07-17 16:17:20 -0400

    Add: rotting-fruit - submission-0

---
+++ Data Structures & Algorithms/rotting-fruit/submission-0.py
@@ -0,0 +1,48 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        fresh = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        time = 0
+
+        queue = deque()
+
+        # Count fresh oranges and store rotten oranges into queue for bfs
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    fresh += 1
+                if grid[r][c] == 2:
+                    queue.append([r,c])
+
+        # def bfs(r,c):
+        #     if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == 0:
+        #         return
+        #     # if its a fresh fruit, make it rotten, decrement fresh and add coordinates to the queue
+        #     if grid[r][c] == 1:
+        #         grid[r][c] = 2
+        #         fresh -= 1
+        #         queue.append([r,c])
+
+        directions = [[1,0],[-1,0],[0,1],[0,-1]] 
+        while queue and fresh > 0:
+            for _ in range(len(queue)):
+                r, c = queue.popleft()
+                for dr, dc in directions:
+                    new_r = r + dr
+                    new_c = c + dc
+                    if new_r < 0 or new_c < 0 or new_r >= rows or new_c >= cols or grid[new_r][new_c] == 0:
+                        continue
+                    if grid[new_r][new_c] == 1:
+                        grid[new_r][new_c] = 2
+                        fresh -= 1
+                        queue.append([new_r, new_c])
+                
+            time += 1
+        
+        return time if fresh == 0 else -1
+        
+
+        
+
+        

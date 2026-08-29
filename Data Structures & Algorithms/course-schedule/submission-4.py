10b4235f4e2f4c4a2c2e41b217c7a43fb15765bf
Abhinav Barat <ab3299@cornell.edu>
2026-08-29 16:28:11 -0400

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,40 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if not prerequisites:
+            return True
+        
+        # Make the preMap hashmap
+        preMap = {i:[] for i in range(numCourses)}
+        for course, preReq in prerequisites:
+            if course not in preMap:
+                preMap[course] = []
+            preMap[course].append(preReq)
+        
+        visited = set()
+        def dfs(crs):
+            if crs in visited:
+                return False
+            
+            if not preMap[crs]:
+                return True
+            
+            visited.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            
+            visited.remove(crs)
+            preMap[crs] = []
+            return True
+
+        
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        
+        return True
+
+        
+        
+
+        

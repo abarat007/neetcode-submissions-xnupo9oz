1a8e8a605e1de522c14147230761c619193eaca5
Abhinav Barat <ab3299@cornell.edu>
2026-09-17 12:30:20 -0400

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        gauss = (n * (n+1)) / 2
+
+        for num in nums:
+            gauss -= num
+        
+        return int(gauss)
+        
